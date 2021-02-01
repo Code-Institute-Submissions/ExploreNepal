a28379c312c9db105645d8a26870df46819c870b
shiva123-coder <sb4_cbu@yahoo.com>
2021-02-01 10:57:45 +0000

    Added .ready function  inside the location.js file.

--- assets/js/location.js
+++ assets/js/location.js
@@ -1,14 +1,23 @@
-        function initMap(){
-           var places = {
-               zoom: 6.15,
-               center: {lat:27.7172, lng:85.3240}
-           } 
-           // Map
-           var map = new google.maps.Map(document.getElementById("map"), places);
-           // Marker
-           var marker =new google.maps.Marker({
-               position:{lat:27.7105, lng:85.3487},
-               map:map
-           });
-        }
-    
+$(document).ready(function () {
+    var locationData = "";
+    $.getJSON("assets/js/location.json", function (locations) {
+         locationData = locations;
+       
+});
+});
+
+
+
+//  function initMap(){
+//            var places = {
+//                zoom: 6.15,
+//                center: {lat:27.7172, lng:85.3240}
+//            } 
+//            // Map
+//            var map = new google.maps.Map(document.getElementById("map"), places);
+//            // Marker
+//            var marker =new google.maps.Marker({
+//                position:{lat:27.7105, lng:85.3487},
+//                map:map
+//            });
+//         
