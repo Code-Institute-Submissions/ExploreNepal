8049afa45d59b224abc7df04a38ea80927ef2825
shiva123-coder <sb4_cbu@yahoo.com>
2021-02-01 11:48:26 +0000

    Added sendMail function to email.js which linked to form inside the contact section of index.html in order for function to execute when user submit the form.

---
+++ assets/js/email.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm){
+    emailjs.send("gmail", "template_c1k39bh", {
+        "from_fname": contactForm.firstname.value,
+         "from_lname": contactForm.lastname.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value
+    })
+
+   .then(
+       function(response){
+           console.log("SUCCESS", response);
+       },
+       function(error){
+           console.log("FAILED", error);
+       });
+       return false;
+}
